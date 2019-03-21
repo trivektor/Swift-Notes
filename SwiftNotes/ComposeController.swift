f1b01cc60b7f1011441a4d8483a70a8853bbc198
Tri Vuong <trivektor@gmail.com>
2019-03-20 23:28:13 -0700

    notes and compose controllers

---
+++ SwiftNotes/ComposeController.swift
@@ -0,0 +1,28 @@
+//
+//  ComposeController.swift
+//  SwiftNotes
+//
+//  Created by Tri Vuong on 3/20/19.
+//  Copyright © 2019 Tri Vuong. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ComposeController: UIViewController, UITextFieldDelegate, UITextViewDelegate {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Compose"
+        let screenSize = UIScreen.main.bounds
+        let screenWidth = screenSize.width
+        let screenHeight = screenSize.height
+        let textField = UITextView(frame: CGRect(x: 0, y: 88, width: screenWidth, height: screenHeight - 88))
+        textField.isEditable = true
+        textField.isScrollEnabled = true
+        textField.backgroundColor = UIColor.white
+        textField.delegate = self
+        textField.font = UIFont.systemFont(ofSize: 18)
+        textField.autocorrectionType = .no
+        self.view.addSubview(textField)
+    }
+}
---
+++ SwiftNotes/NotesController.swift
@@ -0,0 +1,60 @@
+//
+//  NotesController.swift
+//  SwiftNotes
+//
+//  Created by Tri Vuong on 3/15/19.
+//  Copyright © 2019 Tri Vuong. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NotesController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var itemsToLoad : [String] = [];
+    var notesTableView = UITableView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        itemsToLoad.append(contentsOf: ["One", "Two", "Three", "Four", "Five"])
+        self.title = "Notes"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "+",
+            style: .done,
+            target: self,
+            action: #selector(compose)
+        )
+    }
+    
+    @objc public func compose() {
+        let composeController = ComposeController()
+        self.navigationController?.pushViewController(composeController, animated: true)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let screenSize = UIScreen.main.bounds
+        let screenWidth = screenSize.width
+        let screenHeight = screenSize.height
+        
+        notesTableView.frame = CGRect(x: 0, y: 0, width: screenWidth, height: screenHeight)
+        notesTableView.dataSource = self
+        notesTableView.delegate = self
+        
+        notesTableView.register(UITableViewCell.self, forCellReuseIdentifier: "notesCell")
+        
+        self.view.addSubview(notesTableView)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemsToLoad.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "notesCell", for: indexPath)
+        
+        cell.textLabel?.text = itemsToLoad[indexPath.row]
+        
+        return cell
+    }
+}
